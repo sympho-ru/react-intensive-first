778058a1bc19df7e2b6417116ee7053578393701
Alex <alexander.rassokhin@gmail.com>
2019-01-12 19:05:25 +0300

    Initial commit, tried playing with React as shown in the course

--- src/App.js
+++ src/App.js
@@ -2,27 +2,30 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import styled from 'styled-components'
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className="wrapper">
+        <SayFullName name="Alexander" surname="Rassokhin" link="https://google.com" />
+        <SayFullName name="Elena" surname="Rassokhina" link="https://instagr.am" />
+        <SayFullName name="Sophia" surname="Rassokhina" link="#" />
+      </div>      
     );
   }
+}
+
+function SayFullName(props) {
+  return (
+      <div>
+        <h2>My name is {props.name}, surname is {props.surname}</h2>
+        <a href={props.link}>My profile: {props.link}</a>
+      </div>
+    )
 }
 
 export default App;
